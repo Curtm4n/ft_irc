cb8dc704488b8ed93da3ad67c31d64e811b2d4f7
Curtis DAPURIFICACAO <cdapurif@e2r9p7.clusters.42paris.fr>
2023-01-17 11:10:20 +0100

    bind done

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,16 +6,20 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 18:46:52 by cdapurif          #+#    #+#             */
-/*   Updated: 2023/01/16 21:29:34 by cdapurif         ###   ########.fr       */
+/*   Updated: 2023/01/17 09:42:05 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include <cstdlib>
 # include <iostream>
 # include <unistd.h>
+# include <arpa/inet.h>
 # include <sys/socket.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
 
 class Server
 {
--- src/ft_irc.cpp
+++ src/ft_irc.cpp
@@ -6,7 +6,7 @@
 /*   By: cdapurif <cdapurif@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:38:03 by cdapurif          #+#    #+#             */
-/*   Updated: 2023/01/16 22:12:43 by cdapurif         ###   ########.fr       */
+/*   Updated: 2023/01/17 11:08:48 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,38 @@
 void    init_serv(char **av)
 {
     Server      serv; // Useless, need to initiate socket creation (maybe implement most of the features to handle itself)
-    sockaddr    hint;
+    sockaddr_in hint;
     int         sockfd;
+    int         port;
     
     std::cout << "Port: " << av[0] << " | Password: " << av[1] << std::endl;
+
+    // Check validity of port argument
+    port = atoi(av[0]);
+    if (!(port > 0 && port < 65536))
+    {
+        std::cerr << "Invalid port number or argument" << std::endl;
+        return ;
+    }
 
     // Create a socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1)
     {
-        std::cerr << "Can't create a socket, quitting program" << std::endl;
+        std::cerr << "Can't create a socket" << std::endl;
         return ;
     }
 
     // Bind the ip address and port to a socket
-    //hint.sa_family = AF_INET;
-    //hint.sa_data = 
-    //(sockfd, )
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    hint.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sockfd, (sockaddr *)&hint, sizeof(hint)) == -1)
+    {
+        close (sockfd);
+        std::cerr << "Can't bind socket" << std::endl;
+        return ;
+    }
     close(sockfd);
 }
 
